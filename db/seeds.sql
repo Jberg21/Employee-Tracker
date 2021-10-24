839ff2712c8b2ce74ab9ed71b60db7eb3731c9c3
Justin Berg <justinberg@Justins-MBP.localdomain>
2021-10-24 16:41:45 -0600

    update seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -14,17 +14,17 @@ VALUES
 ("Customer Service Manager", "50000", 3),
 ("Customer Service Rep", "40000", 3);
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id, department _id)
 VALUES
-("David", "Hatch", 1, null), 
-("Justin", "Berg", 2, 1), 
-("Wes", "Wilson", 3, null), 
-("Seth", "Robertson", 3, 3), 
-("Kyle", "Griffis", 4, 3),
-("Leslee", "Perkins", 3, 1),
-("Chance", "Russell", 2, 3),
-("Rebecca", "Roberts", 5, 3),
-("Zack", "Byers", 6, 2),
-("Jackie", "Shepard", 7, 9),
-("Felix", "Green", 7, 9)
+("David", "Hatch", 1, null, 1), 
+("Justin", "Berg", 2, 1, 2), 
+("Wes", "Wilson", 3, null, 1), 
+("Seth", "Robertson", 3, 3, 3), 
+("Kyle", "Griffis", 4, 3, 2),
+("Leslee", "Perkins", 3, 1, 3),
+("Chance", "Russell", 2, 3, 3),
+("Rebecca", "Roberts", 5, 3, 2),
+("Zack", "Byers", 6, 2, 1),
+("Jackie", "Shepard", 7, 9, 3),
+("Felix", "Green", 7, 9, 3)
 ;
